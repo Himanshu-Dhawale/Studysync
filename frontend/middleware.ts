c0fa6f418becb10fcf8daa36585bb6de94d615df
Himanshu <himanshudhawale9@gmail.com>
2025-08-27 09:31:15 +0530

    fix: fixed middleware for protected routes

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,6 +1,16 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([
+  '/login(.*)',
+  '/sign-up(.*)',
+  '/',
+]);
+
+export default clerkMiddleware((auth, request) => {
+  if (isProtectedRoute(request)) {
+     auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
@@ -9,4 +19,5 @@ export const config = {
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
+  
 };
